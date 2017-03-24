c803529989dd9ae20f523ee0ccdaf663cd0c115a
Vitalii Protsenko <ext-vitalii.protsenko@here.com>
2017-03-24 14:38:32 +0200

    refactoring

--- lesson13/main.py
+++ lesson13/main.py
@@ -1,9 +1,10 @@
 import my_libs as m
 
+
 def welcome_msg():
     print("What do you want to do?\n"
-          "1 - Check nmber of columns\n"
-          "2 - Check nmber of lines\n"
+          "1 - Check number of columns\n"
+          "2 - Check number of lines\n"
           "3 - Check Headers\n"
           "4 - Check one column (by name or number)\n"
           "5 - Check one column (by number)\n"
@@ -11,54 +12,54 @@ def welcome_msg():
           "7 - Add header\n"
           "8 - Change cell\n"
           "9 - Add line\n"
-          "10 - Write all changest to file\n"
+          "10 - Write all changes to file\n"
           "11 - EXIT\n"
           )
 
-def my_app():
 
-    g=int(input("Your Choice = "))
+def my_app():
+    g = int(input("Your Choice = "))
 
     if g == 1:
-        print("Nmber of columns " + str(m.len_column()))
+        print("Number of columns " + str(m.len_column()))
         return True
     elif g == 2:
-        print("Nmber of lines " + str(m.len_lines()))
+        print("Number of lines " + str(m.len_lines()))
         return True
     elif g == 3:
         print("Headers ( " + str(m.get_headers()) + ")")
         return True
     elif g == 4:
-        i=input('Put column name or index')
+        i = input('Put column name or index')
         print('List of column ' + str(m.get_column(i)))
         return True
     elif g == 5:
-        i=input('Put line number')
+        i = input('Put line number')
         print('Your line: ' + str(m.get_line(i)))
         return True
     elif g == 6:
-        i=input('Put row')
-        j=input('Put line ')
-        print(m.get_cells([i,j]))
+        i = input('Put row')
+        j = input('Put line ')
+        print(m.get_cells([i, j]))
         return True
     elif g == 7:
-        i=input('Put header name')
-        j=input('Put header position index ')
-        print(m.add_header(i,j))
+        i = input('Put header name')
+        j = input('Put header position index ')
+        print(m.add_header(i, j))
         return True
     elif g == 8:
-        i=input('Put row')
-        j=input('Put line ')
-        k=input('Put value of cel ')
-        print(m.set_cell([i,j], k))
+        i = input('Put row')
+        j = input('Put line ')
+        k = input('Put value of cel ')
+        print(m.set_cell([i, j], k))
         return True
     elif g == 9:
-        i=input('Put line index')
-        j=input('Put line value ')
-        print(m.add_line(j,i))
+        i = input('Put line index')
+        j = input('Put line value ')
+        print(m.add_line(j, i))
         return True
     elif g == 10:
-        i=input('Put file name')
+        i = input('Put file name')
         print(m.write_to_file(i))
         return True
     elif g == 11:
@@ -67,17 +68,13 @@ def my_app():
         print('No such choice')
         return False
 
-status=True
+
+status = True
 while status:
-    l='fdsf,sdf'
-    l=list(l.split(','))
-    print(l)
     welcome_msg()
     print("===================================")
-    status=my_app()
+    status = my_app()
     print("===================================")
-
-
 
 '''
 print(m.len_column())
@@ -88,7 +85,7 @@ print(m.get_column('dsf'))
 print(m.get_line(1))
 print(m.get_cells([2,0]))
 print(m.add_header('xz',-3))
-print(m.set_cell([1,"FathegrName"], 'xzxz'))
+print(m.set_cell([1,"FatherName"], 'xzxz'))
 print(m.add_line(['hello',]))
 
 
