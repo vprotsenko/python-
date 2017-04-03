3d868a52018c0aceae4924f37a7e882740454566
Vitalii Protsenko <ext-vitalii.protsenko@here.com>
2017-04-03 22:49:13 +0300

    refactoring

--- homework/lesson15/authorisation/main.py
+++ homework/lesson15/authorisation/main.py
@@ -14,41 +14,63 @@ def sync_var():
 def dump_var_to_db():
     pickle.dump(db, open('user_db', 'wb'))
 
+
 def check_user(login):
     user_list=db.keys()
     if login in user_list:
         return True
 
 
-def add_user(login, password):
+def add_user(login, password, email):
     if check_user(login):
         return False
     else:
-        db.update({login:{'password':password, 'notes':""}})
+        db.update({login:{'password':password, 'email':email}})
+        dump_var_to_db()
         return True
 
 
-
-sync_var()
-
-a = True
-while a:
-
-    if input("Press \"a\" if you want to add user else press enter") == "A":
-        username=input("Put Username")
-        password=input("Put password")
-        notes=input("Put notes")
-        if add_user(username, password) is True:
-            print("User added")
-        else:
-            print("User already exist")
-        a=True
+def authorisation():
+    username=input("Put Username")
+    password=input("Put password")
+    email=input("Put notes")
+    if add_user(username, password, email) is True:
+        print("User added")
     else:
-        a=False
-
-#add_user('vasia', 'test', ['my notes'])
+        print("User already exist")
 
 
-dump_var_to_db()
+def login():
+    username=input("Put Username")
+    password=input("Put password")
+    if username in db.keys():
+        if db[username]['password'] == password:
+            print('welcome')
+            return username
+        else:
+            print('wrong login or password')
+            return None
+    else:
+        print('wrong login or password')
+        return None
+
+def start_screen():
+    sync_var()
+
+    a = True
+    while a:
+        print(db)
+        choice = input("Press \"l\" to login or \"r\" for registration")
+        if choice == 'r':
+            authorisation()
+            a = True
+        elif choice == 'l':
+            login()
+            a = True
+        else:
+            a = False
+
+    dump_var_to_db()
 
 
+start_screen()
