bd00fdad63399d7dcede4ea3cb6bb6733c72a3cf
Vitalii Protsenko <ext-vitalii.protsenko@here.com>
2017-06-24 09:28:51 +0300

    refactoring

--- homework/lesson25/Connect.py
+++ homework/lesson25/Connect.py
@@ -5,3 +5,12 @@ server.listen(5)
 client = server.accept()
 
 client[0].send('sdfsdfsdfdsf'.encode('cp1251'))
+
+while True:
+    message = client[0].recv(65000)
+    if message:
+        print(message)
+
+
+
+
